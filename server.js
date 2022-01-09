1210282f6600243ed8c41de1ec47a9c9cea63a38
Soumyaneel Sarkar <soumyasarkar309@gmail.com>
2022-01-09 11:17:00 +0000

    routes added

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('Server started on port 3000'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/',(req,res)=>{
+    var resJSON = {
+        "res" : "I'm main entry point."
+    }
+    res.json(resJSON);
 })
+
+app.get('/world', (req, res) => {
+    var resJSON = {
+        "res" : "I'm world.!"
+    }
+    res.json(resJSON)
+})
+
 app.get('/hello',(req,res)=>{
-    res.send("hii... I'm hello route");
+    var resJSON = {
+        "res" : "I'm hello."
+    }
+    res.json(resJSON);
 })
